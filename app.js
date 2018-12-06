8b3d4108d3be8bb8a3c10450bbd6445dc8990d35
unknown <w.andrew.pedersen@gmail.com>
2018-12-06 13:34:27 -0700

    DB DONEish

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 6661;
 const queries = require('./queries')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.listen(
-  port,
-  console.log(`Summon the Dark Lord! We are running on PORT ${port} !!!`)
-);
-
+app.get(cors())
+app.get(bodyParser.json())
+app.listen(port, console.log(`Summon the Dark Lord! We are running on PORT ${port} !!!`))
 app.get("/", (req, res) => {
-  queries.listAll().then(bands => {
-    res.send(bands)
-  })
-});
+  queries.listAll().then(bands => res.send(bands))
+})
+app.delete('/:id', (req, res) => {
+  queries.deleteBand(req.params.id).then(res.sendStatus(204))
+})
+app.put('/:id', (req, res) => {
+  queries.updateBand(req.params.id, req.body)
+    .then(band => res.json(band))
+})
+app.post('/', (req, res) => {
+  queries.createBand(req.body).then(res.sendStatus(201))
+})
+app.get("/:id", (req, res) => {
+  queries.getById(req.params.id).then(bands => res.send(bands))
+})
--- queries.js
+++ queries.js
@@ -3,5 +3,17 @@ const db = require('./database-connection')
 module.exports = {
     listAll() {
         return db('bands')
+    },
+    getById(id) {
+        return db('bands').where('id', id)
+    },
+    createBand(pushBand) {
+        return db('bands').insert(pushBand).returning('*')
+    },
+    deleteBand(id) {
+        return db('bands').where('id', id).delete()
+    },
+    updateBand(id, band) {
+        return db('bands').where('id', id).update(band).returning('*')
     }
 }
