729a59290dd12b7202e50a14d9fc08a88cd075db
unknown <w.andrew.pedersen@gmail.com>
2018-12-06 11:02:01 -0700

    app.get added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 6666;
+const port = process.env.PORT || 3336;
 
 app.listen(
   port,
   console.log(`Summon the Dark Lord! We are running on PORT ${port} !!!`)
 );
+
+app.get("/", (req, res) => {
+  res.send("YAYER");
+});
